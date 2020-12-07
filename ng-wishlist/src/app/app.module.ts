e5c6596b9b19f9d7be7cfd25ea77267ae7a6a6a8
Plamen Petrov <biznetmen@gmail.com>
2020-12-07 09:49:23 +0200

    Add MaterialModule for import and export of all Angular Material modules needed and imorts it in the app.module

--- ng-wishlist/src/app/app.module.ts
+++ ng-wishlist/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
