6c960cbbeb9fedd85b69d6372e25271fc8d515dd
Plamen Petrov <biznetmen@gmail.com>
2020-12-08 10:44:08 +0200

    Add @angular/fire and config the environment.ts

--- ng-wishlist/src/environments/environment.ts
+++ ng-wishlist/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDDkvulH4YR7BLLqUNcBPjFkzuwouGjZBg",
+    authDomain: "wishlist-d5160.firebaseapp.com",
+    //databaseURL: '<your-database-URL>',
+    projectId: "wishlist-d5160",
+    storageBucket: "wishlist-d5160.appspot.com",
+    messagingSenderId: "724673296058",
+    appId: "1:724673296058:web:1c262bf45e31f37214b483",
+    //measurementId: '<your-measurement-id>'
+  }
 };
 
 /*
