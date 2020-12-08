83ced76f4a64d83d97511b35b93c7875b64d7f3d
Plamen Petrov <biznetmen@gmail.com>
2020-12-08 09:38:17 +0200

    Add some text and styling to the Welcome page

--- ng-wishlist/src/app/navigation/header/header.component.ts
+++ ng-wishlist/src/app/navigation/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() sidenavToggle = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onToggleSidenav(){
+    this.sidenavToggle.emit();
+
   }
 
 }
--- ng-wishlist/src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ ng-wishlist/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
-  styleUrls: ['./sidenav-list.component.css']
+  styleUrls: ['./sidenav-list.component.css'],
 })
 export class SidenavListComponent implements OnInit {
+  @Output() closeSidenav = new EventEmitter<void>();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onClose() {
+    this.closeSidenav.emit();
   }
-
 }
