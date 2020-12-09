8466f9b77ad99200dc8c4579144c11cbeb069385
Plamen Petrov <biznetmen@gmail.com>
2020-12-09 16:29:46 +0200

    Add tabs in the Wishlist component to display child components

--- ng-wishlist/src/app/material.module.ts
+++ ng-wishlist/src/app/material.module.ts
@@ -9,6 +9,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
     imports:[
@@ -21,7 +23,9 @@ import {MatCardModule} from '@angular/material/card';
         MatToolbarModule,
         MatListModule,
         MatTabsModule,
-        MatCardModule
+        MatCardModule,
+        MatSelectModule,
+        MatDialogModule
     ],
     exports: [
         MatButtonModule,
@@ -33,7 +37,9 @@ import {MatCardModule} from '@angular/material/card';
         MatToolbarModule,
         MatListModule,
         MatTabsModule,
-        MatCardModule
+        MatCardModule,
+        MatSelectModule,
+        MatDialogModule
     ]
 })
 export class MaterialModule {}
