1dee5bb2b71c512517a96a36b2cd606cb1fa4f99
Plamen Petrov <biznetmen@gmail.com>
2020-12-13 05:47:51 +0200

    Firebase config data added to environment.prod.ts

--- ng-wishlist/src/environments/environment.prod.ts
+++ ng-wishlist/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDDkvulH4YR7BLLqUNcBPjFkzuwouGjZBg",
+    authDomain: "wishlist-d5160.firebaseapp.com",
+    //databaseURL: '<your-database-URL>',
+    projectId: "wishlist-d5160",
+    storageBucket: "wishlist-d5160.appspot.com",
+    messagingSenderId: "724673296058",
+    appId: "1:724673296058:web:1c262bf45e31f37214b483",
+    //measurementId: '<your-measurement-id>'
+  }
 };
